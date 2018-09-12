a0d409fcf6edcc76cea6e729133da4922a06e1af
Diego Moreira Guimarães <diegodukao@gmail.com>
2018-09-11 22:19:36 -0400

    setup.py created to make the lib installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Pytheory-testing',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=["pytuning", "numeral", "pygame", "scipy"],
+)
